723b7a6845a56d68ecd1c8b2cfbde8eeea457086
kiaanrani <kiaanrani@gmail.com>
2025-12-12 18:58:13 +0530

    initial

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,6 @@ import { prisma } from "./config/prisma.js";
 import dreamRoutes from "./routes/dreamRoutes.js";
 import sleepRoutes from "./routes/sleepRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
-const PORT = process.env.PORT || 3000;
 
 dotenv.config();
 
@@ -21,5 +20,5 @@ app.use("/dreams", dreamRoutes);
 app.use("/sleep", sleepRoutes);
 
 app.listen(process.env.PORT, () =>
-  console.log("Server running on port", PORT )
+  console.log("Server running on port", process.env.PORT)
 );
